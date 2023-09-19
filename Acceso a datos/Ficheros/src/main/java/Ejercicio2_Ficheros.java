71fb757ec3fafd151c57c534eb2ad762f79e1ff2
Caanosa <danisc2003@gmail.com>
2023-09-19 14:41:32 +0200

    Ejercicio2 ficheros

---
+++ Acceso a datos/Ficheros/src/main/java/Ejercicio2_Ficheros.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Ejercicio2_Ficheros {
+    public static void main(String args[]) throws IOException {
+
+            FileInputStream leer = new FileInputStream("C:\\Users\\FP\\Desktop\\2 DAM\\Acceso a datos\\Ficheros\\Ejercicios\\Ejercicio2\\FicheroByte.txt");
+
+            int byte1;
+
+
+
+
+    }
+}
