e0e6a435fea90de459bef48726c8541d0894b0be
Caanosa <122782648+Caanosa@users.noreply.github.com>
2023-11-15 12:32:05 +0100

    Delete Desarrollo de interaces directory

--- Desarrollo de interaces/UF1/Proyectos/Proyecto1JavaFX/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.proyecto1javafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.proyecto1javafx to javafx.fxml;
-    exports com.mycompany.proyecto1javafx;
-}
